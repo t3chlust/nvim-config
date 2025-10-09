2d9108f46100e2310783651af84c7075cc06975b
t3chlust <t3chlust@gmail.com>
2025-10-09 09:52:14 +0500

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
+vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
+vim.cmd([[colorscheme gruvbox]])
